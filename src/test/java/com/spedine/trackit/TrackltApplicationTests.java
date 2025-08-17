83d8e63d982fbfe9fe8a4969d94526eb51c888f1
Arthur Spedine <tucogamesytthegamesof@gmail.com>
2025-08-17 20:37:12 -0300

    feat: first commit

---
+++ src/test/java/com/spedine/trackit/TrackltApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spedine.trackit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class  TrackItApplicationTests{
+
+    @Test
+    void contextLoads() {
+    }
+
+}
